c27eeab3ae1d6a4dc48c0ddcc8c9f52963439aab
Jarory13 <Jarory13@gmail.com>
2016-06-26 19:23:44 +0000

    Updated HangpersonGame class. Now handles all guessing logic as well as displaying guessed words and maintain game state using turn and win counters.

--- lib/hangperson_game.rb
+++ lib/hangperson_game.rb
@@ -1,5 +1,4 @@
 class HangpersonGame
-
   # add the necessary class methods, attributes, etc. here
   # to make the tests in spec/hangperson_game_spec.rb pass.
 
@@ -7,11 +6,25 @@ class HangpersonGame
 
   # def initialize()
   # end
-  
-  def initialize(word)
-    @word = word
-  end
 
+  attr_accessor :word
+  attr_accessor :guesses
+  attr_accessor :wrong_guesses
+  attr_reader :turn_counter
+  attr_reader :display
+  attr_reader :win_counter
+  
+  def initialize(new_word)
+    @word = new_word.downcase
+    @guesses = ''
+    @wrong_guesses = ''
+    @turn_counter = 0
+    @display = ''
+    @win_counter = 0
+  end
+  
+  
+  
   def self.get_random_word
     require 'uri'
     require 'net/http'
@@ -19,4 +32,46 @@ class HangpersonGame
     Net::HTTP.post_form(uri ,{}).body
   end
 
+
+  #Method to handle guessing logic. 
+  # @param is the letter guessed by the user
+  def guess(letter)
+    raise(ArgumentError, "You must enter a letter") if (letter =~ /[^a-z]/i) 
+     raise(ArgumentError, "You must enter a letter") if !letter || letter.empty?  #for some reason letter.blank? doesn't work. 
+     raise(ArgumentError, "Enter only one letter") if (letter.length > 1)
+    @turn_counter +=1
+    letter.downcase!
+   
+        if (guesses.include? letter) | (wrong_guesses.include? letter) 
+        false
+        else 
+            if word.include? letter
+              @guesses += letter
+              @win_counter +=1
+            else 
+              @wrong_guesses += letter
+            end
+        end
+  end
+
+
+  #Method to display guessed words. Uses "-" to display letters that aren't gussed. 
+  def word_with_guesses
+  display = ''
+    word.each_char do |a|
+      if @guesses.include? a
+          display += a
+      else
+          display += "-"
+      end
+    end
+    display 
+  end
+
+  #Method to check game state. Returns a symbol for :wins, :lose, or keep playing (:play)
+  def check_win_or_lose
+    if win_counter >= word.split("").uniq.length then return :win end
+    @turn_counter >= 7 ? :lose : :play
+  end
+  
 end
